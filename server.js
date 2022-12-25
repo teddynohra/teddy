c4d378c28dee9a08bc408dad60febbab4eb769f7
teddy nohra <tnohra91@gmail.com>
2022-12-26 01:19:07 +0200

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3030;
 app.get('/',function(req,res){
     res.send('hello world');
 })
-app.listen(3000)   
+app.get('/data',function(req,res){
+    var data=[
+        {id:17,name:'teddy'},
+        {id:11,name:'charbel'},
+        {id:17,name:'fawzy'},
+    ]
+    res.send('data');
+})
+app.listen(PORT)   
